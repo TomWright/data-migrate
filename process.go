5cf6771a0c1030cdcbc6aaa4975f92fb7a8b37f9
Tom Wright <contact@tomwright.me>
2019-04-09 14:54:58 +0100

    Rename row process funcs

--- process.go
+++ process.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-func InsertDuplicateRow(insertStmt *sql.Stmt, to *TableContext) ProcessRowFunc {
+func InsertRowFunc(insertStmt *sql.Stmt, to *TableContext) ProcessRowFunc {
 	return func(row *Row) error {
 		var err error
 
@@ -20,14 +20,56 @@ func InsertDuplicateRow(insertStmt *sql.Stmt, to *TableContext) ProcessRowFunc {
 				to.DBName,
 				to.Table,
 				strings.Join(insertColumns, ", "),
-				strings.TrimSuffix(strings.Repeat("?, ", len(insertColumns)), ", ")),
-			)
+				strings.TrimSuffix(strings.Repeat("?, ", len(insertColumns)), ", "),
+			))
 			if err != nil {
 				return err
 			}
 		}
 
 		binds := make([]interface{}, len(row.Columns))
+		for k, v := range row.Columns {
+			binds[k] = v.Value
+		}
+
+		_, err = insertStmt.Exec(binds...)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func UpsertRowFunc(insertStmt *sql.Stmt, to *TableContext) ProcessRowFunc {
+	return func(row *Row) error {
+		var err error
+		if insertStmt == nil {
+			insertColumns := make([]string, len(row.Columns))
+			upsertColumns := make([]string, len(row.Columns))
+			for k, c := range row.Columns {
+				insertColumns[k] = c.Column
+				upsertColumns[k] = c.Column + " = ?"
+			}
+			insertStmt, err = to.DB.Prepare(fmt.Sprintf(
+				"INSERT INTO %s.%s (%s) VALUES(%s) ON DUPLICATE KEY UPDATE %s",
+				to.DBName,
+				to.Table,
+				strings.Join(insertColumns, ", "),
+				strings.TrimSuffix(strings.Repeat("?, ", len(insertColumns)), ", "),
+				strings.Join(upsertColumns, ", "),
+			))
+			if err != nil {
+				return err
+			}
+		}
+
+		binds := make([]interface{}, len(row.Columns)*2)
+		// insert binds
+		for k, v := range row.Columns {
+			binds[k] = v.Value
+		}
+		// upsert binds
 		for k, v := range row.Columns {
 			binds[k] = v.Value
 		}
